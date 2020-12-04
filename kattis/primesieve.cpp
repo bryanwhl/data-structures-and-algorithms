61669875c8cb62b040daf17c07f61242351bf0a3
bryanwhl <42731945+bryanwhl@users.noreply.github.com>
2020-12-04 23:59:43 +0800

    Update primesieve.cpp

--- kattis/primesieve.cpp
+++ kattis/primesieve.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+//https://open.kattis.com/problems/primesieve
 //Not an optimized solution, but AC nonetheless.
 //This problem gave me lots of trouble because I did my first sieve solution with vector<int> instead of vector<bool> and it gave me MLE.
 //I miscalculated by not realizing that simply changing my vector type will allow my solution to be AC, so I went on to try segmented sieve just to TLE.
